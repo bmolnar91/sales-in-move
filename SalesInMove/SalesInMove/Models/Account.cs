7db8366b0347de8b2cf6dd54a89642e475429b37
Alex Seres <lilaalex95@gmail.com>
2021-01-27 09:11:34 +0000

    user controller file has been implemented with usermanager and signin manager, now we just have to test database and sign in

--- SalesInMove/SalesInMove/Models/Account.cs
+++ SalesInMove/SalesInMove/Models/Account.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SalesInMove.Models
 {
-    public class Account
+    public class Account: IdentityUser 
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }

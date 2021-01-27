d5ddf4e6d539559326f12df2e0fd1a039fbfc4a3
Lilla Kertesz <lil.kertesz@gmail.com>
2021-01-27 12:59:59 +0100

    Register google service in startup

--- SalesInMove/SalesInMove/Startup.cs
+++ SalesInMove/SalesInMove/Startup.cs
@@ -47,6 +47,15 @@ namespace SalesInMove
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddAuthentication().AddGoogle(options =>
+            {
+            IConfigurationSection googleAuthNSection =
+                Configuration.GetSection("Authentication:Google");
+
+            options.ClientId = googleAuthNSection["ClientId"];
+            options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

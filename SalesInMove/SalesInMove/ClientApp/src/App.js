81098d71ffd0d6dc8fcf732952dd52cc868fb071
Marcell Jeney <marcijeney@gmail.com>
2021-01-31 14:01:32 +0100

    homepage design update

--- SalesInMove/SalesInMove/ClientApp/src/App.js
+++ SalesInMove/SalesInMove/ClientApp/src/App.js
@@ -1,41 +1,65 @@
-import React, { Component } from "react";
-import { Route } from "react-router";
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import { HomePage } from "./pages/home/home";
-import { RegistrationMain } from "./pages/registration/registration-main";
-import { RegistrationEmployer } from "./pages/registration/registration-employer";
-import { RegistrationEmployee } from "./pages/registration/registration-employee";
-import { LoginPage } from "./pages/login/login";
-import { StickyFooter } from "./components/StickyFooter";
-import { AppBar } from "./components/AppBar";
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import { HomePage } from './pages/home/home';
+import { RegistrationMain } from './pages/registration/registration-main';
+import { RegistrationEmployer } from './pages/registration/registration-employer';
+import { RegistrationEmployee } from './pages/registration/registration-employee';
+import { LoginPage } from './pages/login/login';
+import { StickyFooter } from './components/StickyFooter';
+import { AppBar } from './components/AppBar';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Image from './images/homepageBg.jpg';
 
-export default class App extends Component {
-  static displayName = App.name;
+const useStyles = makeStyles((theme) => ({
+  mainGridItem: {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: 'cover',
+  },
+  fullWidthContainerMain: {
+    marginLeft: '100px',
+    marginTop: '150px',
+  },
+}));
 
-  render() {
-    return (
-      <Box>
+const App = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      direction='row'
+      spacing={1}
+      className={classes.mainGridItem}
+    >
+      <Grid xs={12}>
         <AppBar />
-        <Container maxWidth="md" style={{ minHeight: "100vh" }}>
+      </Grid>
+      <Grid xs={12}>
+        <Container maxWidth='xl' className={classes.fullWidthContainerMain}>
           <Box my={4}>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/regisztracio" component={RegistrationMain} />
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/regisztracio' component={RegistrationMain} />
             <Route
               exact
-              path="/regisztracio/munkaltato"
+              path='/regisztracio/munkaltato'
               component={RegistrationEmployer}
             />
             <Route
               exact
-              path="/regisztracio/munkavallalo"
+              path='/regisztracio/munkavallalo'
               component={RegistrationEmployee}
             />
-            <Route exact path="/bejelentkezes" component={LoginPage} />
+            <Route exact path='/bejelentkezes' component={LoginPage} />
           </Box>
         </Container>
+      </Grid>
+      <Grid xs={12}>
         <StickyFooter />
-      </Box>
-    );
-  }
-}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
--- SalesInMove/SalesInMove/ClientApp/src/components/HomeMainSection/HomeMainSection.jsx
+++ SalesInMove/SalesInMove/ClientApp/src/components/HomeMainSection/HomeMainSection.jsx
@@ -1,29 +1,98 @@
-import React from "react";
+import React from 'react';
 import {
   Container,
   MidSectionContainer,
   Image,
   TextContainer,
-} from "./HomeMainSection.style";
+} from './HomeMainSection.style';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  mainh1Text: {
+    fontWeight: '600',
+    fontSize: '4rem',
+  },
+  mainSecondaryText: {
+    fontSize: '32px',
+  },
+  moveText: {
+    fontSize: '42px',
+  },
+  jobSearchBtn: {
+    marginRight: '30px',
+  },
+}));
 
 export const HomeMainSection = () => {
-  const paragraph = `Mi jut eszedbe a következő szavakról? Munkakeresés és a pozícióra a lefmegfelelőbb embert megtalálása? Hosszú, unalmas, stresszes, fárasztó, irritáló. Nagyon kevés választ kaptam mely szerint érdekes, szórakoztató vagy akár kihívásokkal teli.
-
-Mi úgy gondoltuk, hogy legyen ez a stresszes feladat egy jó kikapcsolódás. Mutasd meg szeméyiségedet azonnal .... önéletrajzod csak később lesz fontos. 
-  
-Merj önmagad lenni. Felkészültél??
-  
-MOVE`;
-
+  const classes = useStyles();
   return (
-    <Container>
-      <MidSectionContainer>
-        <TextContainer>
-          <p>{paragraph}</p>
-        </TextContainer>
-        <Image src="/images/landing-01.png" />
-      </MidSectionContainer>
-      <br />
-    </Container>
+    <Grid container direction='column' spacing={1}>
+      <Grid container item xs={12}>
+        <Grid item item xs={5}>
+          <Container component='div'>
+            <TextContainer>
+              <Typography
+                className={classes.mainh1Text}
+                variant='h2'
+                component='h2'
+                gutterBottom
+              >
+                Mutasd meg szeméyiségedet azonnal, merj önmagad lenni!
+              </Typography>
+              <Typography className={classes.mainSecondaryText} gutterBottom>
+                Mi megkönnyítjük a munka keresést és a pozícióra legmegfelelőbb
+                ember megtalálását!
+              </Typography>
+              <Typography
+                className={classes.moveText}
+                variant='h4'
+                component='h4'
+                gutterBottom
+              >
+                Felkészültél ? Move !
+              </Typography>
+            </TextContainer>
+          </Container>
+        </Grid>
+        <Grid xs={7}></Grid>
+      </Grid>
+      <Grid container xs={5}>
+        <Grid item className={classes.buttonContainer}>
+          <Button
+            variant='contained'
+            color='primary'
+            className={classes.jobSearchBtn}
+          >
+            Munkát keresek !
+          </Button>
+          <Button variant='contained' color='secondary'>
+            Munkát kínálok !
+          </Button>
+        </Grid>
+        <Grid xs={7}></Grid>
+      </Grid>
+    </Grid>
+
+    // <Container>
+    //   <MidSectionContainer>
+    //     <Container component='div'>
+    //       <TextContainer>
+    //         <p>{paragraph}</p>
+    //       </TextContainer>
+    //     </Container>
+    //     <Container component='div'>
+    //       <Button variant='contained' color='primary'>
+    //         Primary
+    //       </Button>
+    //       <Button variant='contained' color='primary'>
+    //         Primary
+    //       </Button>
+    //     </Container>
+    //   </MidSectionContainer>
+    //   <br />
+    // </Container>
   );
 };

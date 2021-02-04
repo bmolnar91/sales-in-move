87bdba24d69a167f1aff6bcd25702a5374997a0b
Alex Seres <lilaalex95@gmail.com>
2021-02-04 15:33:57 +0100

    faked usermanager

---
+++ SalesInMove/NUnitTests/Backend/FakeSignInManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SalesInMove.NUnitTests.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests.Backend
+{
+    public class FakeSignInManager :SignInManager<IdentityUser>
+    {
+        //this fakes usermanager and it will be added to the mocking famework
+        public FakeSignInManager() : base(new FakeUserManager(),
+                                        new HttpContextAccessor(),
+                                        new Mock<IUserClaimsPrincipalFactory<IdentityUser>>().Object,
+                                        new Mock<IOptions<IdentityOptions>>().Object,
+                                        new Mock<ILogger<SignInManager<IdentityUser>>>().Object,
+                                       new Mock<IAuthenticationSchemeProvider>().Object,
+                                       new Mock<IUserConfirmation<IdentityUser>>().Object
+                                       )
+        { }
+    }
+}

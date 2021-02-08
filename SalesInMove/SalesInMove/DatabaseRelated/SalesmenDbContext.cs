fd165078acda721404e672f8e5435cf14579ac72
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-09 00:40:16 +0100

    Add Company and User table seed

--- SalesInMove/SalesInMove/DatabaseRelated/SalesmenDbContext.cs
+++ SalesInMove/SalesInMove/DatabaseRelated/SalesmenDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SalesInMove.Models;
+using System.Collections.Generic;
+using System;
 
 namespace SalesInMove.DatabaseRelated
 {
@@ -21,6 +23,36 @@ namespace SalesInMove.DatabaseRelated
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            #region Company seed
+            builder.Entity<User>().HasData(
+                new User
+                {
+                    Id = "0",
+                    Email = "marko@gmail.com",
+                    Password = "Asd123",
+                    UserType = 2,
+                }
+            );
+
+            builder.Entity<Company>().HasData(
+                new Company
+                {
+                    CompanyId = 12,
+                    Name = "Markoó Kft.",
+                    Headquarter = "Halásztelek",
+                    TaxNumber = 24767107243,
+                    Registry = 0109321480,
+                    YearOfFoundation = DateTime.Parse("Jan 1, 2000"),
+                    NumberOfSalesman = 10,
+                    AnnualNettoIncome = 2000321865,
+                    EmployeeOpinions = new string[] { "Good", "Bad", "Terrific!", "Horrific!" },
+                    SalesSupport = true,
+                    UserId = "0"
+
+                }
+            );
+            #endregion
 
         }
 

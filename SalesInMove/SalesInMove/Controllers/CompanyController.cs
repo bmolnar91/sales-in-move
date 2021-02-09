0452b19407f4871d35fd5d56a7baf49676401e48
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-09 16:39:22 +0100

    Configure Json options

--- SalesInMove/SalesInMove/Controllers/CompanyController.cs
+++ SalesInMove/SalesInMove/Controllers/CompanyController.cs
@@ -1,9 +1,14 @@
+using System.Text;
+using System.ComponentModel;
+using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesInMove.DatabaseRelated;
 using SalesInMove.Models;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 namespace SalesInMove.Controllers
 {
@@ -11,17 +16,22 @@ namespace SalesInMove.Controllers
     [ApiController]
     public class CompanyController : ControllerBase
     {
-        private readonly IAsyncCompanyRepository _repo;
+        private readonly ICompanyRepository _repo;
+        private readonly JsonSerializerOptions _jsonOptions;
 
-        public CompanyController(IAsyncCompanyRepository repo)
+        public CompanyController(ICompanyRepository repo)
         {
             _repo = repo;
+            _jsonOptions = new JsonSerializerOptions {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.All)
+            };
         }
 
         [HttpGet("getcompany/{name}")]
         public string Get(string name)
         {
-            return JsonSerializer.Serialize(_repo.GetCompanyByName(name));
+
+            return JsonSerializer.Serialize(_repo.GetCompanyByName(name), _jsonOptions);
         }
 
     }

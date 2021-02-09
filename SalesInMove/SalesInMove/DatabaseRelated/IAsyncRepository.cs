75843d3b8ff723e167b41c485975cb15b629d3a0
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-09 10:43:32 +0100

    Change GetAllSync return type in IAsyncRepository interface

--- SalesInMove/SalesInMove/DatabaseRelated/IAsyncRepository.cs
+++ SalesInMove/SalesInMove/DatabaseRelated/IAsyncRepository.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
+
 namespace SalesInMove.DatabaseRelated
 {
     public interface IAsyncRepository<TEntity>
     {
          public TEntity AddAsync(TEntity entity);
          public TEntity UpdateAsync(TEntity entity);
-         public TEntity GetAllAsync(TEntity entity);
+         public IEnumerable<TEntity> GetAllAsync(TEntity entity);
          public TEntity GetByEmailAsync(TEntity entity);
          public TEntity DeleteByEmailAsync(TEntity entity);
     }

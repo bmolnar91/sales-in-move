359abbaa8e0b2830e08efb67217b6f275df2fd59
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-09 17:36:47 +0100

    Remove User.EmailAddress

--- SalesInMove/SalesInMove/Models/User.cs
+++ SalesInMove/SalesInMove/Models/User.cs
@@ -8,9 +8,6 @@ namespace SalesInMove.Models
     {
         public int UserType { get; set; }
 
-        [EmailAddress]
-        public string EmailAddress { get; set; }
-
         [Required]
         public string Password { get; set; }
 

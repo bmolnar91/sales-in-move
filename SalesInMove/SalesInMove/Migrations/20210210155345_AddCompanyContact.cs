87e06767ac49e593a9f78ec3b8ff8aec6980f547
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-10 16:57:33 +0100

    Add migration scripts for company Contact fields

---
+++ SalesInMove/SalesInMove/Migrations/20210210155345_AddCompanyContact.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesInMove.Migrations
+{
+    public partial class AddCompanyContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactFirstName",
+                table: "Companies",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactLastName",
+                table: "Companies",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "0",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "e2704c75-4d64-4fc8-8628-f100a13ee3fc", "d7d19d50-250b-4be1-8893-2a81bb111eaf" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactFirstName",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "ContactLastName",
+                table: "Companies");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "0",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "dbff3fee-b160-40b0-ac32-5add4d384132", "ff7314d5-b1d7-4751-b578-4d7d6a1ce41e" });
+        }
+    }
+}

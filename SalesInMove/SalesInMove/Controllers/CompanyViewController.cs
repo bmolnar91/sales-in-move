10204e91516fb90a8c26b49ea60bf180ba2d037e
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-10 16:54:57 +0100

    Add company profile view

--- SalesInMove/SalesInMove/Controllers/CompanyViewController.cs
+++ SalesInMove/SalesInMove/Controllers/CompanyViewController.cs
@@ -1,18 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using SalesInMove.DatabaseRelated;
+using SalesInMove.Models;
 
 namespace SalesInMove.Controllers
 {
     [Route("company/[action]")]
     public class CompanyViewController : Controller
     {
+        private readonly ICompanyRepository _companyRepo;
+
+        public CompanyViewController(ICompanyRepository companyRepo)
+        {
+            _companyRepo = companyRepo;
+        }
+
         public IActionResult Register()
         {
             return View();
         }
         
-        public IActionResult Profile()
+        public IActionResult Profile(string companyName)
         {
-            return View();
+            Company company = _companyRepo.GetCompanyByName(companyName);
+            
+            return View(company);
         }
     }
 }

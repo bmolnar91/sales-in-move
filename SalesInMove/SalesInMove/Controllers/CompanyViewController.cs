2bf0aa03e72bb399529ad708a743e110911e2868
Márton Szabó <mrthn_sz4bo@pop-os.localdomain>
2021-02-10 15:05:33 +0100

    Add CompanyView controller

---
+++ SalesInMove/SalesInMove/Controllers/CompanyViewController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesInMove.Controllers
+{
+    [Route("company/[action]")]
+    public class CompanyViewController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}

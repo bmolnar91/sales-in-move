653adeb5fb4c3d387bec5f083bb7b512dd446bb7
bmolnar91 <balintkilencvenegy@gmail.com>
2021-02-10 17:17:54 +0100

    Save progress

--- SalesInMove/SalesInMove/ClientApp/src/App.js
+++ SalesInMove/SalesInMove/ClientApp/src/App.js
@@ -1,7 +1,5 @@
-import React, { Component } from "react";
-import { Route } from "react-router";
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
+import React, { useEffect } from "react";
+import { Route, Switch } from "react-router";
 import { HomePage } from "./pages/home/home";
 import { RegistrationMain } from "./pages/registration/registration-main";
 import { RegistrationEmployer } from "./pages/registration/registration-employer";
@@ -12,23 +10,27 @@ import { AppBar } from "./components/AppBar";
 import profilePage from "./pages/profile/profilePage";
 
 const App = () => {
+  useEffect(() => {}, []);
+
   return (
     <>
       <AppBar />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/regisztracio" component={RegistrationMain} />
-      <Route
-        exact
-        path="/regisztracio/munkaltato"
-        component={RegistrationEmployer}
-      />
-      <Route
-        exact
-        path="/regisztracio/munkavallalo"
-        component={RegistrationEmployee}
-      />
-      <Route exact path="/bejelentkezes" component={LoginPage} />
-      <Route exact path="/ProfilePage" component={profilePage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/regisztracio" component={RegistrationMain} />
+        <Route
+          exact
+          path="/regisztracio/munkaltato"
+          component={RegistrationEmployer}
+        />
+        <Route
+          exact
+          path="/regisztracio/munkavallalo"
+          component={RegistrationEmployee}
+        />
+        <Route exact path="/bejelentkezes" component={LoginPage} />
+        <Route exact path="/ProfilePage" component={profilePage} />
+      </Switch>
       {/* <StickyFooter /> */}
     </>
   );

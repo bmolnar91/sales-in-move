e15415d21b9db3894861c2c5af6d4f2abdec4e98
Marcell Jeney <marcijeney@gmail.com>
2021-02-11 17:42:44 +0100

    search components ready

--- SalesInMove/SalesInMove/ClientApp/src/App.js
+++ SalesInMove/SalesInMove/ClientApp/src/App.js
@@ -1,38 +1,37 @@
-import React, { Component } from "react";
-import { Route } from "react-router";
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import { HomePage } from "./pages/home/home";
-import { RegistrationMain } from "./pages/registration/registration-main";
-import { RegistrationEmployer } from "./pages/registration/registration-employer";
-import { RegistrationEmployee } from "./pages/registration/registration-employee";
-import { LoginPage } from "./pages/login/login";
-import { StickyFooter } from "./components/StickyFooter";
-import { AppBar } from "./components/AppBar";
-import profilePage from "./pages/profile/profilePage";
-import "./styles/App.css";
-import { SearchEmployees } from "./components/SearchEmployees/SearchEmployees";
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import { HomePage } from './pages/home/home';
+import { RegistrationMain } from './pages/registration/registration-main';
+import { RegistrationEmployer } from './pages/registration/registration-employer';
+import { RegistrationEmployee } from './pages/registration/registration-employee';
+import { LoginPage } from './pages/login/login';
+import { StickyFooter } from './components/StickyFooter';
+import { AppBar } from './components/AppBar';
+import profilePage from './pages/profile/profilePage';
+import './styles/App.css';
+import { SearchEmployees } from './components/SearchEmployees/SearchEmployees';
 
 const App = () => {
   return (
     <>
       <AppBar />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/regisztracio" component={RegistrationMain} />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/regisztracio' component={RegistrationMain} />
       <Route
         exact
-        path="/regisztracio/munkaltato"
+        path='/regisztracio/munkaltato'
         component={RegistrationEmployer}
       />
       <Route
         exact
-        path="/regisztracio/munkavallalo"
+        path='/regisztracio/munkavallalo'
         component={RegistrationEmployee}
       />
-      <Route exact path="/bejelentkezes" component={LoginPage} />
-      <Route exact path="/ProfilePage" component={profilePage} />
-      <Route exact path="/search-employees" component={SearchEmployees} />
-      <StickyFooter />
+      <Route exact path='/bejelentkezes' component={LoginPage} />
+      <Route exact path='/ProfilePage' component={profilePage} />
+      <Route exact path='/search-employees' component={SearchEmployees} />
     </>
   );
 };

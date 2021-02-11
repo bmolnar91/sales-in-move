1b59969b7e11f195553045aac40156a133b35f5e
Márton Szabó <makotoimx@gmail.com>
2021-02-11 22:26:19 +0100

    Create employee search service

---
+++ SalesInMove/SalesInMove/Services/EmployeeSearch.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using SalesInMove.DatabaseRelated;
+using SalesInMove.Models;
+using SalesInMove.ViewModels;
+
+namespace SalesInMove.Services
+{
+    public class EmployeeSearch : IEmployeeSearchService
+    {
+        private readonly IEmployeeSearchRepository _empSearchRepo;
+        private readonly IEmployeeSearchVMFactory _employeeSearchVMFactory;
+
+        public EmployeeSearch(IEmployeeSearchRepository searchRepository, IEmployeeSearchVMFactory employeeSearchVMFactory)
+        {
+            _empSearchRepo = searchRepository;
+            _employeeSearchVMFactory = employeeSearchVMFactory;
+        }
+        public List<EmployeeSearchVM> GetEmployees(SearchFormVM searchForm)
+        {
+            IEnumerable<Employee> employees = _empSearchRepo.GetResults(searchForm.Education, searchForm.City, searchForm.Languages);
+            
+            //List<EmployeeVM> vMs = new List<EmployeeVM>();
+            List<EmployeeSearchVM> empVMs = new List<EmployeeSearchVM>();
+
+            foreach (Employee employee in employees)
+            {
+                empVMs.Add(_employeeSearchVMFactory.CreateVM(employee.FirstName, employee.LastName, employee.Age, employee.ProfilePicture, employee.City, employee.Specialisation, employee.Education, employee.SalesTurnOver, employee.Languages));
+            }
+
+            return empVMs;
+            
+        }
+
+        public string GetString(SearchFormVM searchForm)
+        {
+            return _empSearchRepo.GetString(searchForm.Languages);
+        }
+    }
+}

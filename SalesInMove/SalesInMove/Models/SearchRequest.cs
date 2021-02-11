262a948b194bf1849cdfaae1bf158e5bad0d1bba
Alex Seres <lilaalex95@gmail.com>
2021-02-11 14:06:04 +0100

    added class where we can turn the request into object

---
+++ SalesInMove/SalesInMove/Models/SearchRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesInMove.Models
+{
+    public class SearchRequest
+    {
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
--- SalesInMove/SalesInMove/Services/SearchForPotentialWorker.cs
+++ SalesInMove/SalesInMove/Services/SearchForPotentialWorker.cs
@@ -8,6 +8,41 @@ namespace SalesInMove.Services
 {
     public class SearchForPotentialWorker : ISearch
     {
+        private List<SearchRequest> requestes = new List<SearchRequest>
+        {
+            new SearchRequest
+            {
+                Title = "EletKor",
+                Status = "mandatory",
+                Type = "int",
+                Value = "22"
+            },
+            new SearchRequest
+            {
+                Title = "Varos",
+                Status = "mandatory",
+                Type = "string",
+                Value = "Budapest"
+            },
+            new SearchRequest
+            {
+                Title = "Jogositvany",
+                Status = "optional",
+                Type = "bool",
+                Value = "true"
+            }
+        };
+         
+
+        public void CheckMandatories()
+        {
+            foreach(var request in requestes)
+            {
+
+            }
+        }
+               
+         
         public List<Account> Search(Company company, Dictionary<string, bool> parameters)
         {
 
